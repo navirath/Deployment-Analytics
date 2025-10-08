71bfb5185a8350eb3c65d9453faafa80f7c44b1e
navirath <navirathnavi@gmail.com>
2025-10-08 23:45:03 +0530

    ram and cpu calculating files were deleted

--- src/index.js
+++ src/index.js
@@ -4,30 +4,9 @@ import { simpleGit } from "simple-git";
 import { generate } from "./util.js";
 import path from "path";
 import { getAllFiles } from "./file.js";
-import { estimateRAM } from "./estimateRam.js";
 import { createClient } from "redis";
-import { benchmarkWithCPU } from "./cpu.js";
 const publisher = createClient();
 publisher.connect();
-
-
-
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-// import fs from "fs";
-
-// const s3 = new S3Client({
-//   region: "us-east-1",
-//   endpoint: "http://localhost:4566",  // LocalStack S3 endpoint
-//   forcePathStyle: true,               // important for LocalStack
-//   credentials: {
-//     accessKeyId: "test",
-//     secretAccessKey: "test"
-//   }
-// });
-
-// const BUCKET = "my-local-bucket"; // create this bucket once with AWS CLI
-
-
 
 const PORT = 4000;
 const app = express();
@@ -42,35 +21,8 @@ app.post("/deploy", async (req, res) => {
     const id = generate();
     await simpleGit().clone(repoUrl,  `../output/${id}`);
     const files = getAllFiles( `../output/${id}`);
-  
 
-//     for (const file of files) {
-//   const relativePath = path.relative(`../output/${id}`, file);
-//   await uploadFileToS3(file, `${id}/${relativePath}`);
-// }
-
-    publisher.lPush("build-queue", id);
-    
-    console.log(estimateRAM(`../output/${id}`));
-
-
-//     async function runTest() {
-//   try {
-
-//     // Change URL to your running project
-//     const results = await benchmarkWithCPU("http://localhost:3000", 15, 2, 50);
-
-//     console.log("📊 Benchmark Results:");
-//     console.log("wrk output:\n", results.wrkOutput);
-//     console.log("CPU usage:", results.cpuUsage);
-//     console.log("Memory usage:", results.memoryUsage);
-//   } catch (err) {
-//     console.error("❌ Error running benchmark:", err);
-//   }
-// }
-
-// runTest();
-        
+    publisher.lPush("build-queue", id);    
 
     res.json({
         id: id
